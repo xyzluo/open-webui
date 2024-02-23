16e02a98a4500f0c0e94fad25b9724859724d561
garyyizhou.luo <quantumabacus1@gmail.com>
2024-02-22 21:54:54 -0500

    modified startup script

--- run-ollama-docker.sh
+++ run-ollama-docker.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 host_port=11434
+# host_port=5050
 container_port=11434
+# container_port=5050
 
 read -r -p "Do you want ollama in Docker with GPU support? (y/n): " use_gpu
 
 docker rm -f ollama || true
-docker pull ollama/ollama:latest
+# docker pull ollama/ollama:latest
 
-docker_args="-d -v ollama:/root/.ollama -p $host_port:$container_port --name ollama ollama/ollama"
+docker_args=(-d -v ollama:/root/.ollama -p $host_port:$container_port --name ollama ollama)
 
-if [ "$use_gpu" = "y" ]; then
-    docker_args="--gpus=all $docker_args"
+if [ "$use_gpu" == "y" ]; then
+    docker_args+=("--gpus=all")
 fi
 
-docker run $docker_args
+printf "Running: docker run %s\n" "${docker_args[*]}"
+
+docker run "${docker_args[@]}"
 
 docker image prune -f
